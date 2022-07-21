dfc62c8950ae6671fa2eb68edb647d50c5bf58c9
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-07-21 17:05:14 +0200

    Added some style and status object

--- app.js
+++ app.js
@@ -2,46 +2,55 @@ const packages = {
     Package1: {
         FirstName: 'John',
         Label: 'Toys 4 kids',
+        Status: 'Handling...',
         TrackerID: 1234567890
     },
     Package2: {
         FirstName: 'Charlie',
         Label: 'Plush toy',
+        Status: 'Handling...',
         TrackerID: 1234567890
     },
     Package3: {
         FirstName: 'Johnny',
         Label: 'Photo frames',
+        Status: 'Handling...',
         TrackerID: 1234567890
     },
     Package4: {
         FirstName: 'Sandra',
         Label: 'Toys 4 kids',
+        Status: 'Handling...',
         TrackerID: 1234567890
     },
     Package5: {
         FirstName: 'Tina',
         Label: 'Plush toy',
+        Status: 'Handling...',
         TrackerID: 1234567890
     },
     Package6: {
         FirstName: 'Eva',
         Label: 'Books',
+        Status: 'Handling...',
         TrackerID: 1234567890
     },
     Package7: {
         FirstName: 'Anne',
         Label: 'Car accesorie',
+        Status: 'Handling...',
         TrackerID: 1234567890
     },
     Package8: {
         FirstName: 'Sammy',
         Label: 'Books',
+        Status: 'Handling...',
         TrackerID: 1234567890
     },
     Package9: {
         FirstName: 'Johnathan',
         Label: 'Massage kit',
+        Status: 'Handling...',
         TrackerID: 1234567890
     }
 };
