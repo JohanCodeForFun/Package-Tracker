39ee6a71a61cce829cb03dbdf6cf81e97b8ad917
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-07-21 16:49:21 +0200

    packages array bundled

---
+++ app.js
@@ -0,0 +1,47 @@
+const packages = {
+    Package1: {
+        FirstName: 'John',
+        Label: 'Toys 4 kids',
+        TrackerID: 1234567890
+    },
+    Package2: {
+        FirstName: 'Charlie',
+        Label: 'Plush toy',
+        TrackerID: 1234567890
+    },
+    Package3: {
+        FirstName: 'Johnny',
+        Label: 'Photo frames',
+        TrackerID: 1234567890
+    },
+    Package4: {
+        FirstName: 'Sandra',
+        Label: 'Toys 4 kids',
+        TrackerID: 1234567890
+    },
+    Package5: {
+        FirstName: 'Tina',
+        Label: 'Plush toy',
+        TrackerID: 1234567890
+    },
+    Package6: {
+        FirstName: 'Eva',
+        Label: 'Books',
+        TrackerID: 1234567890
+    },
+    Package7: {
+        FirstName: 'Anne',
+        Label: 'Car accesorie',
+        TrackerID: 1234567890
+    },
+    Package8: {
+        FirstName: 'Sammy',
+        Label: 'Books',
+        TrackerID: 1234567890
+    },
+    Package9: {
+        FirstName: 'Johnathan',
+        Label: 'Massage kit',
+        TrackerID: 1234567890
+    }
+};
