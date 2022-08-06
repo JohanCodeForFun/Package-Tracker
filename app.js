e0b6b5cc62ce2d183944a68d4261eec07d3836e6
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-08-07 01:21:52 +0200

    copied over code form branch, search-for-last-four because of merge failure

--- app.js
+++ app.js
@@ -86,70 +86,36 @@ const packages = [
 function trackPackage(input) {
     input = +document.getElementById('trackerInput').value;
 
-        //reset fields with multiple searches
-    document.getElementById('status').textContent = '';
-    document.getElementById('firstName').textContent = '';
-    document.getElementById('label').textContent = '';
-    document.getElementById('delivered').textContent = '';
-    document.getElementById('trackerID').textContent = '';
+    const filteredPackages = packages.find(x => {
+        const id = x.TrackerID.toString();
+        const lastFourDigits = id.slice(-4);
+        return Number(lastFourDigits) === input;
+    });
 
-    const descPackage = document.getElementById('desc')
-    descPackage.textContent = 'Searching package...';
-    descPackage.setAttribute('id', 'play-loading');
+    let lastFourResult = filteredPackages;
 
     if (packages.find(({TrackerID}) => TrackerID === input)) {
-
-        setTimeout(() => {
-            const packageData = packages.find(({TrackerID}) => TrackerID === input);
-            descPackage.textContent = 'Package details: ';
-            descPackage.setAttribute('id', 'desc');
-            document.getElementById('firstName').textContent = 'First name: ' + packageData.FirstName;
-            document.getElementById('label').textContent = 'Contents: ' + packageData.Label;
-            document.getElementById('status').textContent = 'Status: ' + packageData.Status;
-            document.getElementById('delivered').textContent = 'Delivered: ' + packageData.Delivered;
-            document.getElementById('trackerID').textContent = 'TrackerID: ' + packageData.TrackerID;
-            return packageData;
-        
-        }, 3000);
-
+        const packageData = packages.find(({TrackerID}) => TrackerID === input);
+        document.getElementById('desc').textContent = 'Package details: ';
+        document.getElementById('firstName').textContent = 'First name: ' + packageData.FirstName;
+        document.getElementById('label').textContent = 'Contents: ' + packageData.Label;
+        document.getElementById('status').textContent = 'Status: ' + packageData.Status;
+        document.getElementById('delivered').textContent = 'Delivered: ' + packageData.Delivered;
+        document.getElementById('trackerID').textContent = 'TrackerID: ' + packageData.TrackerID;
+        return packageData;
     } else {
-        setTimeout(() => {
-            descPackage.textContent = 'Package not found.';
-            descPackage.setAttribute('id', 'desc');
-            document.getElementById('firstName').textContent = '';
-            document.getElementById('label').textContent = '';
-            document.getElementById('status').textContent = 'Try again or contact support for help.';
-            document.getElementById('delivered').textContent = '';
-            document.getElementById('trackerID').textContent = '';
-        }, 3000);      
+        document.getElementById('desc').textContent = 'Package not found.';
+        document.getElementById('firstName').textContent = '';
+        document.getElementById('label').textContent = '';
+        document.getElementById('status').textContent = 'Try again or contact support for help.';
+        document.getElementById('delivered').textContent = '';
+        document.getElementById('trackerID').textContent = '';
     }
-};
 
-// function to show and hide loading bar
-
-
-// function onLoadStyle() {
-//     document.querySelector('#progress-container').style.display='none';
-//     document.querySelector('#progress-bar').style.display='none';
-// }
-
-let restartButton = document.querySelector('#restartAnimation');
-restartButton.addEventListener('click', restartAnimation, false);
-
-function restartAnimation(event) {
-    const progressBar = document.querySelector('.progress-bar');
-
-    progressBar.setAttribute('id', 'filler');
-    document.getElementById('#progress-container').style.display='';
-    document.getElementsByClassName('#progress-bar').style.display='';
-    
-
-    requestAnimationFrame(() => {
-        progressBar.setAttribute('id', 'play-animation');
-
-        setTimeout(() => {
-            progressBarContainer.style.display='none';
-            progressBarLoading.style.display='none';
-        }, 3000);
-    })
+    document.getElementById('desc').textContent = 'Package details: ';
+    document.getElementById('firstName').textContent = 'First name: ' + lastFourResult.FirstName;
+    document.getElementById('label').textContent = 'Contents: ' + lastFourResult.Label;
+    document.getElementById('status').textContent = 'Status: ' + lastFourResult.Status;
+    document.getElementById('delivered').textContent = 'Delivered: ' + lastFourResult.Delivered;
+    document.getElementById('trackerID').textContent = 'TrackerID: ' + lastFourResult.TrackerID;
 };
